4d77fe18ed1fd00b65753b5f7e9e698767c46ea0
A.Lapshin <polick@mail.ru>
2024-04-05 15:35:18 +0200

    Add tests for App

--- src/test/java/com/example/movie/PostgresApplicationTests.java
+++ src/test/java/com/example/movie/PostgresApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.movie;
 
+import com.example.movie.movie.entity.MovieEntity;
 import com.example.movie.movie.repository.MovieRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class PostgresApplicationTests {
@@ -19,8 +22,19 @@ class PostgresApplicationTests {
 	void repositoryTest() {
 		// Напишите простой тест работы movieRepository.
 		// Вам необходимо убедиться, что сохраниение и поиск по id проходят корректно
+		MovieEntity movie = MovieEntity.builder()
+				.title("11")
+				.description("22")
+				.build();
+		movie = movieRepository.save(movie);
 
-		assert false;
+		Optional<MovieEntity> optionalMovieEntity = movieRepository.findById(movie.getId());
+		assert optionalMovieEntity.isPresent();
+
+		MovieEntity check = optionalMovieEntity.get();
+		assert check.getId().equals(movie.getId());
+
+		// assert false;
 	}
 
 }
--- src/test/java/com/example/movie/WebTests.java
+++ src/test/java/com/example/movie/WebTests.java
@@ -1,5 +1,7 @@
 package com.example.movie;
 
+import com.example.movie.movie.dto.request.CreateMovieRequest;
+import com.example.movie.movie.dto.request.EditMovieRequest;
 import com.example.movie.movie.entity.MovieEntity;
 import com.example.movie.movie.repository.MovieRepository;
 import com.example.movie.movie.routes.MovieRoutes;
@@ -15,8 +17,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -35,7 +39,7 @@ public class WebTests {
 
     @BeforeEach
     public void config() {
-        for(MovieEntity movie : MovieGenerator.list()){
+        for (MovieEntity movie : MovieGenerator.list()) {
             movieRepository.save(movie);
         }
     }
@@ -58,39 +62,106 @@ public class WebTests {
 
     @Test
     void createTest() throws Exception {
-        // Напишите тест, который проверяет корректность работы запроса MovieApiController.create
-        assert false;
+        // Напишите тест, который проверяет корректность работы запроса
+        // MovieApiController.create
+        // assert false;
+        CreateMovieRequest request = CreateMovieRequest.builder()
+                .title("createTest")
+                .description("createTest")
+                .build();
+        mockMvc.perform(post(MovieRoutes.CREATE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(content().string(containsString("createTest")));
     }
 
     @Test
     void findByIdTest() throws Exception {
         // Напишите тест, который проверяет корректность работы запроса MovieApiController.byId
-        assert false;
+        // assert false;
+        MovieEntity movie = MovieEntity.builder()
+                .title("findByIdTest")
+                .description("findByIdTest")
+                .build();
+        movieRepository.save(movie);
+        mockMvc.perform(get(MovieRoutes.BY_ID, movie.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("findByIdTest")));
     }
 
     @Test
     void findById_NotFound_Test() throws Exception {
         // Напишите тест, который проверяет корректность ответа запроса MovieApiController.byId, когда элемент не найдет.
         // запрос должен возвращать 404 статус
-        assert false;
+        // assert false;
+        mockMvc.perform(get(MovieRoutes.BY_ID, 123456789)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 
     @Test
     void updateTest() throws Exception {
         // Напишите тест, который проверяет корректность работы запроса MovieApiController.edit
-        assert false;
+        // assert false;
+        MovieEntity movie = MovieEntity.builder()
+                .title("11")
+                .description("22")
+                .build();
+        movie = movieRepository.save(movie);
+
+        EditMovieRequest editMovieRequest = EditMovieRequest.builder()
+                .title("updateTest")
+                .description("updateTest")
+                .build();
+        mockMvc.perform(put(MovieRoutes.EDIT, movie.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(editMovieRequest)))
+                .andDo(print())
+                .andExpect(content().string(containsString("updateTest")));
     }
 
     @Test
     void deleteTest() throws Exception {
         // Напишите тест, который проверяет корректность работы запроса MovieApiController.delete
-        assert false;
+        // assert false;
+        MovieEntity movie = MovieEntity.builder()
+                .title("11")
+                .description("22")
+                .build();
+        movieRepository.save(movie);
+        assert movieRepository.findById(movie.getId()).isPresent();
+
+        mockMvc.perform(delete(MovieRoutes.BY_ID, movie.getId().toString()))
+                .andDo(print())
+                .andExpect(status().isOk());
+        assert movieRepository.findById(movie.getId()).isEmpty();
     }
 
     @Test
     void searchTest() throws Exception {
         // Напишите тест, который проверяет корректность работы запроса MovieApiController.search
-        assert false;
+        // assert false;
+        List<MovieEntity> result = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            MovieEntity movie = MovieEntity.builder()
+                    .title("title_" + i)
+                    .description("descr_" + i)
+                    .build();
+
+            result.add(movieRepository.save(movie));
+        }
+        mockMvc.perform(get(MovieRoutes.SEARCH)
+                        .param("size", "1000")
+                        .param("query", "title_")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(result)));
     }
 
     @Test
@@ -101,11 +172,16 @@ public class WebTests {
                 "Невидимый Мир",
                 "Группа ученых раскрывает существование параллельного мира, где законы физики искажены, и начинают исследование этой невидимой реальности."));
 
-
         // Напишите тест, который проверяет корректность работы запроса MovieApiController.search,
         // при заранее созданных данных.
         // PS: код выше удалять не нужно
-
-        assert false;
+        // assert false;
+        mockMvc.perform(get(MovieRoutes.SEARCH)
+                        .param("size", "1000")
+                        .param("query", "Невидимый")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(check)));
     }
 }
